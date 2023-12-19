b663be0b1cb1e422c1738dcdab7f235566b79d8f
InspiredcL <56697862+InspiredcL@users.noreply.github.com>
2023-12-19 16:57:56 -0300

    Update increase_cluster.sh

--- 06_dataproc/increase_cluster.sh
+++ 06_dataproc/increase_cluster.sh
@@ -1,2 +1,2 @@
 gcloud dataproc clusters update ch6cluster\
-   --num-secondary-workers=3 --num-workers=4 --region=us-central1
+   --num-secondary-workers=3 --num-workers=4 --region=southamerica_west1
