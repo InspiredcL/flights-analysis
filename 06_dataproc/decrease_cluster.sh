e116f07cd250d7e23b6fffe7fa1f8e15ca18b28a
InspiredcL <56697862+InspiredcL@users.noreply.github.com>
2023-12-19 16:58:40 -0300

    Update decrease_cluster.sh

--- 06_dataproc/decrease_cluster.sh
+++ 06_dataproc/decrease_cluster.sh
@@ -1,2 +1,2 @@
 gcloud dataproc clusters update ch6cluster\
-   --num-secondary-workers=0 --num-workers=2 --region=us-central1
+   --num-secondary-workers=0 --num-workers=2 --region=southamerica-west1
