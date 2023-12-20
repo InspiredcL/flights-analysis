9554abf6d17a750912604d432a76359202ea0ae7
InspiredcL <56697862+InspiredcL@users.noreply.github.com>
2023-12-20 18:14:53 -0300

    Update ingest_from_crsbucket.sh

--- 04_streaming/ingest_from_crsbucket.sh
+++ 04_streaming/ingest_from_crsbucket.sh
@@ -13,7 +13,7 @@ TO=gs://$BUCKET/flights/tzcorr
 #sharded files
 CMD="gsutil -m cp "
 for SHARD in `seq -w 0 26`; do
-  CMD="$CMD ${FROM}/all_flights-000${SHARD}-of-00026"
+    CMD="$CMD ${FROM}/all_flights-000${SHARD}-of-00026"
 done
 CMD="$CMD $TO"
 echo $CMD
@@ -22,8 +22,8 @@ $CMD
 # load tzcorr into BigQuery
 PROJECT=$(gcloud config get-value project)
 bq --project_id $PROJECT \
-  load --source_format=NEWLINE_DELIMITED_JSON --autodetect ${PROJECT}:dsongcp.flights_tzcorr \
-  ${TO}/all_flights-*
+    load --source_format=NEWLINE_DELIMITED_JSON --autodetect ${PROJECT}:dsongcp.flights_tzcorr \
+    ${TO}/all_flights-*
 
 cd transform
 
