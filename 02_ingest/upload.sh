f74b7ca9a03dd5a5868f941b214c8735fc9f6389
Inspired <56697862+InspiredcL@users.noreply.github.com>
2023-12-25 19:39:59 -0300

    Update upload.sh

--- 02_ingest/upload.sh
+++ 02_ingest/upload.sh
@@ -10,5 +10,6 @@ BUCKET=$1
 
 echo "Uploading to bucket $BUCKET..."
 gsutil -m cp *.csv gs://$BUCKET/flights/raw/
-#gsutil -m acl ch -R -g allUsers:R gs://$BUCKET/flights/raw
-#gsutil -m acl ch -R -g google.com:R gs://$BUCKET/flights/raw
+# Permisos
+gsutil -m acl ch -R -g allUsers:R gs://$BUCKET/flights/raw
+gsutil -m acl ch -R -g google.com:R gs://$BUCKET/flights/raw
